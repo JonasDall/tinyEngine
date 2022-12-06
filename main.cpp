888604b8dc0327077dce028ca085dbb730afc93f
JonasDall <jonaskd@live.no>
2022-12-06 20:44:08 +0100

    Removed unneccesaries

--- main.cpp
+++ main.cpp
@@ -2,14 +2,13 @@
 #include <vector>
 
 #include "source/tiny.hpp"
+#include "lib/tileson.hpp"
 
 int main()
 {
-    /*
     tiny::TinyEngine engine({256, 240}, "Game", "asset/Map.json");
     
     engine.Run();
-    */
 
     // engine.LoadLevel("asset/Map.json");
 
